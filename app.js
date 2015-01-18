171f813844092c51da48522d49b01ec331dedb1e
Jose Miguel Parrella <j@bureado.com>
2015-01-18 23:08:53 +0000

    Average/consensus based logic in production for Jan 18 game

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var azure = require('azure');
 var WebSocket = require('ws');
+var math = require('mathjs');
 
 var config = require("./config.js");
 
@@ -14,74 +15,85 @@ var AzureTopic = config.topic;
 var pixPerSec = 20;
 var timeStep=1e3/pixPerSec;
 var stationScalar = 3.20793 * Math.pow(10,5) * 9.8;
-var MaxScale = 1.15; // new high if n% more than previous max
-var MaxStale = 30000; // n seconds stale time for a recorded max
-var MaxPackets = 5; // n packets before declaring an event
-var curMax = 0;
-var msgMax = {};
-var jumpcounter = 0;
+var MaxScale = 1.03; // scale
+var AnalyzeSpan = 250; // packets
 var lastNotification = new Date();
-var minWait = 60;
+var maxNotification = 60; // prevent more than 1 notif/minute
+var EventSpan = 2500; // ms
 
-socket.on('message', function(channel, msg) {
+// Utility variables
+var previousAvg = {}, recentAvg = {}, jumpTracker = {};
+
+socket.on('message', function(msg) {
 	var packet = JSON.parse(msg);
+
 	if (!packet.data) {
 		return;
 	}
+
         var _decimate = packet.samprate / pixPerSec;
         var _i = 0;
         var _t = packet.starttime;
-	if (msgMax[makeChanKey(packet)]){ // not our first packet
-		if (msgMax[makeChanKey(packet)].hasOwnProperty('value')) { // not our first max
-			curMax = msgMax[makeChanKey(packet)].value;
-		}
-	} else {
-		msgMax[makeChanKey(packet)] = {};
-	}
+	var sta = makeChanKey(packet).substr(0,4);
+
         while (_i < packet.data.length) { // iterate thru all datapoints
             var _index = Math.round(_i += _decimate);
             if (_index < packet.data.length) {
 		var curVal = Math.abs(packet.data[_index] / stationScalar); // this is the value we need
-		if ( curVal > curMax * MaxScale ){ // if it jumps...
-			msgMax[makeChanKey(packet)] = {value: curVal, timestamp: _t, reason: 'scale', previous: curMax}; // we have a new max!
+		
+		if (!recentAvg[sta]){
+			recentAvg[sta] = [];
 		}
-		if ( _t >= msgMax[makeChanKey(packet)].timestamp + MaxStale ) { // if it stinks...
-			msgMax[makeChanKey(packet)] = {value: curVal, timestamp: _t, reason: 'stale', previous: curMax}; // we have a new max!
+		recentAvg[sta].push(curVal);
+		if(recentAvg[sta].length > AnalyzeSpan){
+			currentAvg = math.mean(recentAvg[sta]);
+			if(!previousAvg[sta]) {
+				previousAvg[sta] = currentAvg;
+			}
+			console.log("Mean for " + AnalyzeSpan + " packets in sta " + sta + " is " + math.round(currentAvg, 6) + " previous was " + math.round(previousAvg[sta], 6));
+			if (currentAvg > (previousAvg[sta]*MaxScale)) {
+				console.log(new Date(_t).toString());
+				console.log("That's a " + currentAvg/previousAvg[sta] + " increase! Threshold: " + MaxScale);
+				jumpTracker[sta] = _t;
+			} else {
+				if(new Date() > (jumpTracker[sta] + EventSpan)) {
+					console.log("Forgetting stale data for " + sta + " formerly: " + Object.keys(jumpTracker).length);
+					delete jumpTracker[sta];
+				}
+			}
+			previousAvg[sta] = currentAvg;
+			delete recentAvg[sta];
+
+//
+			if (Object.keys(jumpTracker).length >= 2) { // we might have an event
+				var dt = new Date();
+				console.log("Event! " + dt.toString());
+				if ((dt-lastNotification)/1e3>maxNotification) {
+					var txt = "Event detected: " + dt.toString();
+					var pk = { notification: txt, show: true };
+					serviceBusService.sendTopicMessage(AzureTopic, { body: JSON.stringify(pk) }, function(error) {
+					      if (error) { console.log(error); } else { console.log(pk); }
+					});
+					lastNotification = dt;
+				} else {
+					console.log("...but skipping notification due to wait period.");
+				}
+				jumpTracker = {};
+			}
+//
+
 		}
+
                 _t += timeStep;
             }
         }
-
-	// Let's analyze what happened with stations in this packet
-	var alljump = true;
-	for (var key in msgMax) { // analyze all stations
-		if (msgMax[key].hasOwnProperty('reason') && jumpcounter == 0) {
-			alljump = alljump && ((msgMax[key].reason == 'scale') && msgMax[key].previous > 0);
-		}
-	}
-	if (alljump) { // we might have an event
-		++jumpcounter;
-		if (jumpcounter >= MaxPackets) { // we have an event
-			var dt = new Date();
-			if ((dt-lastNotification)/1e3<60) {
-				var txt = "Shake detected: " + dt.toString;
-				var pk = { notification: txt, show: true };
-				serviceBusService.sendTopicMessage(AzureTopic, { body: JSON.stringify(pk) }, function(error) {
-				      if (error) { console.log(error); } else { console.log(pk); }
-				});
-				lastNotification = dt;
-			} else {
-				console.log("Skipping event due to wait period");
-			}
-			jumpcounter = 0;
-		}
-	}
 });
 
 // Shake utilities
 makeTimeKey = function(t) {
 	return parseInt(t / timeStep, 0) * timeStep;
 };
+
 makeChanKey = function(packet) {
         //remove the dashes that are the default for loc = null
         var loc = (!packet.loc || packet.loc == "--" || this.loc == "") ? "" : ("_" + packet.loc);
